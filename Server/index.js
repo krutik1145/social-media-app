85bd5cf0ba5fa8d5f60abacf7227f908746eac65
krutik jain <krutikjain26@gmail.com>
2025-06-17 02:38:10 +0530

    fixed coral for vercel

--- Server/index.js
+++ Server/index.js
@@ -31,7 +31,7 @@ app.use(cors({
    
      origin: [
         'http://localhost:3000',
-        "https://social-media-app-coral-sigma.vercel.app/"],
+        "https://social-media-app-coral-sigma.vercel.app"],
 }));
 
 
